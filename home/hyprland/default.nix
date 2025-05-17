45b528607f06a2407ee8616a2a41bf7fa3f3df1a
vxrail <superskateboarder@outlook.com>
2025-05-17 17:37:24 +0200

    init moveall to ws (wip)

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [
     ./ashell.nix
     ./clipse.nix
@@ -9,6 +9,23 @@
     enable = true;
     settings = {
       "$mod" = "SUPER";
+      binds = builtins.concatLists (builtins.genLists (
+          i: let
+            ws = toString (i + 1);
+          in [
+            "$mod, ${ws}, workspace, ${ws}"
+            "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
+            "$mod CTRL, ${ws}, exec, ${pkgs.writers.writeNuBin "moveTo${ws}" ''
+              let state = hyprctl clients -j | from json;
+              let windows = $state | where workspace.id == ${ws};
+
+              for window in $windows {
+                hyprctl dispath movetoworkspacesilent $"${ws},address:($window.adress)"
+              }
+            ''}"
+          ]
+        )
+        9);
     };
   };
 }
